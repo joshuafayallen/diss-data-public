ae518f691c46a764eae42bea9cc12a9a03606c46
joshuafayallen <joshua.f.allen@gmail.com>
2025-07-08 17:06:19 -0400

    Updating the script to scrape the website bettter

--- src/scrapper-greyblueguppy.py
+++ src/scrapper-greyblueguppy.py
@@ -1,11 +1,22 @@
+# this will run the scrapper 
+
 from greyblueguppy.mdjfscrapper import Scraper
+import polars as pl
+import string
 
 
-sc = Scraper()
+# this is a little bit more robust since the MDJF is not the most well constructed website 
+# so if you just submit a eventually it will go blank once it has a combo of letters that it doesn't have
+# For example FM has no entries so the webpage will go blank and then the scrapper will think its done with the letter A 
+lets1 = pl.DataFrame({'first_let': list(string.ascii_uppercase)})
+lets2  = pl.DataFrame({'second_let': list(string.ascii_uppercase)})
 
-# scrapes just the letter p 
-just_the_letter_p = sc.scrape_letter(letter = 'P')
+lets_sub = lets1.join(lets2, how='cross').with_columns(
+    pl.concat_str(['first_let', 'second_let']).alias('submission')
+)['submission'].to_list()
 
-# scrapes the full memorial 
+sc = Scraper(headless=True,
+             lets_sub=lets_sub)
 
-all_lets = sc.scrape_all()
+
+mdjf = sc.scrape_all()
